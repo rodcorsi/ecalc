4029be531748cf325377726767b03664ac181725
Rodrigo Corsi <rodrigocorsi@gmail.com>
2025-09-11 08:39:28 -0300

    add solver test for ParseExpresstion

--- esolver/scanner.go
+++ esolver/scanner.go
@@ -4,8 +4,8 @@ import (
 	"bufio"
 	"bytes"
 	"io"
-	"strings"
 	"unicode"
+	"unicode/utf8"
 )
 
 type Scanner struct {
@@ -32,17 +32,52 @@ func (s *Scanner) Unread() {
 	_ = s.r.UnreadRune()
 }
 
+func (s *Scanner) Peek(nRunes int) []rune {
+	if nRunes <= 0 {
+		return nil
+	}
+	var runes []rune
+	for i := 0; i < nRunes; i++ {
+		b, err := s.r.Peek(i + 1)
+		if err != nil {
+			return runes
+		}
+		firstByte := b[i]
+
+		var size int
+		// Determina o tamanho do rune a partir do primeiro byte.
+		if firstByte < utf8.RuneSelf {
+			size = 1
+		} else if firstByte&0xE0 == 0xC0 {
+			size = 2
+		} else if firstByte&0xF0 == 0xE0 {
+			size = 3
+		} else if firstByte&0xF8 == 0xF0 {
+			size = 4
+		} else {
+			return runes
+		}
+		b, err = s.r.Peek(i + size)
+		if err != nil {
+			return runes
+		}
+		r, _ := utf8.DecodeRune(b[i:])
+		runes = append(runes, r)
+	}
+	return runes
+}
+
 func (s *Scanner) Scan() Token {
 	ch := s.Read()
-	if IsNumber(ch) {
+	if isNumber(ch) {
 		s.Unread()
 		return s.ScanNumber()
 	} else if unicode.IsLetter(ch) {
 		s.Unread()
 		return s.ScanWord()
-	} else if IsOperator(ch) {
+	} else if isOperator(ch) {
 		return Token{OPERATOR, string(ch)}
-	} else if IsWhitespace(ch) {
+	} else if isWhitespace(ch) {
 		s.Unread()
 		return s.ScanWhitespace()
 	}
@@ -62,44 +97,49 @@ func (s *Scanner) Scan() Token {
 func (s *Scanner) ScanWord() Token {
 	var buf bytes.Buffer
 	buf.WriteRune(s.Read())
-	value := buf.String()
-
-	for v, tokenType := range s.elemNames {
-		for {
-			if value == v {
-				return Token{tokenType, value}
-			}
-
-			if strings.HasPrefix(v, value) {
-				ch := s.Read()
-				if ch == eof {
-					break
-				}
-
-				buf.WriteRune(ch)
-				value = buf.String()
-			} else {
-				break
-			}
-		}
-	}
-
-	return Token{CONSTANT, value}
-}
-
-func (s *Scanner) ScanNumber() Token {
-	var buf bytes.Buffer
-	buf.WriteRune(s.Read())
 
 	for {
 		if ch := s.Read(); ch == eof {
 			break
-		} else if !IsNumber(ch) && !IsDegree(ch) {
+		} else if !unicode.IsLetter(ch) {
 			s.Unread()
 			break
 		} else {
 			_, _ = buf.WriteRune(ch)
 		}
+	}
+	value := buf.String()
+	if tt, ok := s.elemNames[value]; ok {
+		return Token{tt, value}
+	}
+
+	return Token{ERROR, value}
+}
+
+func (s *Scanner) ScanNumber() Token {
+	var buf bytes.Buffer
+	for {
+		runes := s.Peek(1)
+		if len(runes) == 0 || runes[0] == eof {
+			s.Read()
+			break
+		}
+		ch := runes[0]
+		if isNumber(ch) {
+			s.Read()
+			_, _ = buf.WriteRune(ch)
+			continue
+		}
+		if isDegree(ch) {
+			nextRunes := s.Peek(2)
+			if len(nextRunes) > 1 && unicode.IsLetter(nextRunes[1]) {
+				break
+			}
+			s.Read()
+			_, _ = buf.WriteRune(ch)
+			continue
+		}
+		break
 	}
 
 	return Token{NUMBER, degToDecString(buf.String())}
@@ -112,7 +152,7 @@ func (s *Scanner) ScanWhitespace() Token {
 	for {
 		if ch := s.Read(); ch == eof {
 			break
-		} else if !IsWhitespace(ch) {
+		} else if !isWhitespace(ch) {
 			s.Unread()
 			break
 		} else {
@@ -123,18 +163,18 @@ func (s *Scanner) ScanWhitespace() Token {
 	return Token{WHITESPACE, buf.String()}
 }
 
-func IsNumber(r rune) bool {
+func isNumber(r rune) bool {
 	return unicode.IsDigit(r) || r == '.'
 }
 
-func IsDegree(r rune) bool {
+func isDegree(r rune) bool {
 	return r == 'd' || r == '\'' || r == 'm' || r == '"' || r == 's'
 }
 
-func IsOperator(r rune) bool {
+func isOperator(r rune) bool {
 	return r == '+' || r == '-' || r == '*' || r == '/' || r == '^'
 }
 
-func IsWhitespace(ch rune) bool {
+func isWhitespace(ch rune) bool {
 	return ch == ' ' || ch == '\t' || ch == '\n'
 }
--- esolver/solver_test.go
+++ esolver/solver_test.go
@@ -2,10 +2,11 @@ package esolver
 
 import (
 	"math/big"
+	"reflect"
 	"testing"
 )
 
-func TestSolve(t *testing.T) {
+func Test_esolver_Solve(t *testing.T) {
 	assert := func(value string, expected *big.Float, isError bool) {
 
 		x, err := New().Solve(value)
@@ -20,13 +21,6 @@ func TestSolve(t *testing.T) {
 			t.Errorf("Value:`%v` expected:`%v` result:`%v`", value, expected, x)
 		}
 	}
-	// assert("/2", 0, false)
-	// assert("", -1, true)
-	// assert(".2.", -1, true)
-	// assert("1.2.", -1, true)
-	// assert(".2.1", -1, true)
-	// assert("1.2.3", -1, true)
-	// assert("-", -1, true)
 
 	assert(".", big.NewFloat(0), false)
 	assert("1", big.NewFloat(1), false)
@@ -53,13 +47,74 @@ func TestSolve(t *testing.T) {
 	assert(`45m`, degToDec(`45m`), false)
 	assert(`45s`, degToDec(`45s`), false)
 
+	assert(`5sin90`, big.NewFloat(5), false)
+
 	assert(`tan45d15'25"`, tan(degToDec(`45d15'25"`)), false)
 	assert(`tan45d15'25"+5`, new(big.Float).Add(tan(degToDec(`45d15'25"`)), big.NewFloat(5)), false)
 	assert(`tan45d15'25"*5`, new(big.Float).Mul(tan(degToDec(`45d15'25"`)), big.NewFloat(5)), false)
 	assert(`5tan45`, new(big.Float).Mul(big.NewFloat(5), tan(degToDec(`45`))), false)
-	assert(`tantan30`, tan(tan(big.NewFloat(30))), false)
+	assert(`tan(tan30)`, tan(tan(big.NewFloat(30))), false)
 	assert(`pi3`, new(big.Float).Mul(consts["pi"](), big.NewFloat(3)), false)
-	assert(`pipi`, new(big.Float).Mul(consts["pi"](), consts["pi"]()), false)
+	assert(`pi*pi`, new(big.Float).Mul(consts["pi"](), consts["pi"]()), false)
 	assert(`5(5)`, big.NewFloat(25), false)
 	assert(`(5)(5)`, big.NewFloat(25), false)
+}
+
+func Test_esolver_ParseExpression(t *testing.T) {
+	tests := []struct {
+		name    string
+		s       string
+		want    Stack
+		wantErr bool
+	}{
+		{"literal", "1", Stack{[]Token{{NUMBER, "1"}}}, false},
+		{"literal more numbers", "100", Stack{[]Token{{NUMBER, "100"}}}, false},
+		{"addition", "1+2", Stack{[]Token{{NUMBER, "1"}, {OPERATOR, "+"}, {NUMBER, "2"}}}, false},
+		{"subtraction", "3-1", Stack{[]Token{{NUMBER, "3"}, {OPERATOR, "-"}, {NUMBER, "1"}}}, false},
+		{"multiplication", "2*3", Stack{[]Token{{NUMBER, "2"}, {OPERATOR, "*"}, {NUMBER, "3"}}}, false},
+		{"division", "4/2", Stack{[]Token{{NUMBER, "4"}, {OPERATOR, "/"}, {NUMBER, "2"}}}, false},
+		{"exponent", "2^3", Stack{[]Token{{NUMBER, "2"}, {OPERATOR, "^"}, {NUMBER, "3"}}}, false},
+		{"parentheses", "(1+2)", Stack{[]Token{{LPAREN, "("}, {NUMBER, "1"}, {OPERATOR, "+"}, {NUMBER, "2"}, {RPAREN, ")"}}}, false},
+		{"function", "sin(90)", Stack{[]Token{{FUNCTION, "sin"}, {LPAREN, "("}, {NUMBER, "90"}, {RPAREN, ")"}}}, false},
+		{"constant", "pi", Stack{[]Token{{CONSTANT, "pi"}}}, false},
+		{"unary minus", "-1", Stack{[]Token{{OPERATOR, "-"}, {NUMBER, "1"}}}, false},
+		{"unary plus", "+1", Stack{[]Token{{OPERATOR, "+"}, {NUMBER, "1"}}}, false},
+		{"complex expression", "5.5*pi+sin(90-10)", Stack{[]Token{
+			{NUMBER, "5.5"},
+			{OPERATOR, "*"},
+			{CONSTANT, "pi"},
+			{OPERATOR, "+"},
+			{FUNCTION, "sin"},
+			{LPAREN, "("},
+			{NUMBER, "90"},
+			{OPERATOR, "-"},
+			{NUMBER, "10"},
+			{RPAREN, ")"},
+		}}, false},
+		{"implicit multiplication constant", "3pi", Stack{[]Token{{NUMBER, "3"}, {OPERATOR, "*"}, {CONSTANT, "pi"}}}, false},
+		{"implicit multiplication function", "3sin(90)", Stack{[]Token{{NUMBER, "3"}, {OPERATOR, "*"}, {FUNCTION, "sin"}, {LPAREN, "("}, {NUMBER, "90"}, {RPAREN, ")"}}}, false},
+		{"implicit multiplication parentheses", "3(4)", Stack{[]Token{{NUMBER, "3"}, {OPERATOR, "*"}, {LPAREN, "("}, {NUMBER, "4"}, {RPAREN, ")"}}}, false},
+		{"implicit multiplication parentheses 2", "(3)4", Stack{[]Token{{LPAREN, "("}, {NUMBER, "3"}, {RPAREN, ")"}, {OPERATOR, "*"}, {NUMBER, "4"}}}, false},
+		{"implicit multiplication parentheses 3", "(3)(4)", Stack{[]Token{{LPAREN, "("}, {NUMBER, "3"}, {RPAREN, ")"}, {OPERATOR, "*"}, {LPAREN, "("}, {NUMBER, "4"}, {RPAREN, ")"}}}, false},
+		{"dms", `45d15'25"`, Stack{[]Token{{NUMBER, `45.2569444444444444444444444444444444444444444444444444444444444444444444444444`}}}, false},
+		{"tokenizer test", "1+*2", Stack{[]Token{{NUMBER, "1"}, {OPERATOR, "+"}, {OPERATOR, "*"}, {NUMBER, "2"}}}, false},
+	}
+	e := New()
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, gotErr := e.ParseExpression(tt.s)
+			if gotErr != nil {
+				if !tt.wantErr {
+					t.Errorf("ParseExpression() failed: %v", gotErr)
+				}
+				return
+			}
+			if tt.wantErr {
+				t.Fatal("ParseExpression() succeeded unexpectedly")
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("ParseExpression() = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
